03c12112973de39f922de9d62f9e961b85c7b53b
Arlington Corpuz <Arlingto.Corpuz001@student.weltec.ac.nz>
2021-03-26 13:42:02 +1300

    This is a change that I have made, after adi has cloned from Github. So from here onwards we are doing it seperately.

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -10,9 +10,7 @@ namespace Assignment1
 
             Console.WriteLine("Hello! please enter the first member's name:");
             names = Console.ReadLine();
-            Console.WriteLine("please enter the second member's name:");
-            names += " and " + Console.ReadLine();
-
+            
             Console.WriteLine("Hello " + names);
         }
     }
