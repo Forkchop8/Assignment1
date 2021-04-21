db43179de7498e3f5714daf142b36fcc68c9dc55
AdityaRao <aditya.rao01@student.weltec.ac.nz>
2021-04-21 18:54:23 +1200

    Added questiona about where we study

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -7,16 +7,17 @@ namespace Assignment1
         static void Mains(string[] args)
         {
             String names;
+            String names1;
 
             Console.WriteLine("Hello! please enter the first member's name:");
             names = Console.ReadLine();
             Console.WriteLine("please enter the second member's name:");
             names += " and " + Console.ReadLine();
             Console.WriteLine("Where do you study?");
-            names = Console.ReadLine();
+            names1 = Console.ReadLine();
 
             Console.WriteLine("Hello " + names);
-            Console.WriteLine("Enjoy your studies at" + names);
+            Console.WriteLine("Enjoy your studies at" + names1);
         }
     }
 }
